89cb8065a16dfcc3d7c8f53897c2d2d26db5219f
dibranica <erjon.dibrani@cloudastro.de>
2026-06-22 08:26:38 +0200

    feat: bump tf and provider version

--- terrafrom.tf
+++ terrafrom.tf
@@ -1,3 +1,9 @@
 terraform {
-  required_version = "~> 1.9.0"
+  required_version = ">= 1.15"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 4.77"
+    }
+  }
 }
